7ac87021d957912311b52936228c6e50249639c3
Nico <nico99rueda@gmail.com>
2026-04-06 12:02:34 +0200

    feat: use avatar mask and upper body layer to walk while attacking

--- Assets/_Project/Scripts/Combat/MeleeCombat.cs
+++ Assets/_Project/Scripts/Combat/MeleeCombat.cs
@@ -13,18 +13,16 @@ namespace MobaGameplay.Combat
             entity = GetComponent<BaseEntity>();
         }
 
-        public override void BasicAttack()
-        {
-            // 1. Detenemos el movimiento al lanzar el puñetazo
-            if (entity.Movement != null)
-            {
-                entity.Movement.Stop();
-            }
+public override void BasicAttack()
+{
+    // Ya no detenemos el movimiento aquí para poder caminar y atacar a la vez.
+    // if (entity.Movement != null)
+    // {
+    //     entity.Movement.Stop();
+    // }
 
-            // 2. Disparamos el evento (esto avisará al animador)
-            base.BasicAttack();
-
-            // Aquí en el futuro añadiremos lógica de cooldowns y daño
-        }
+    // Disparamos el evento (esto avisará al animador)
+    base.BasicAttack();
+}
     }
 }
